769b5dbad8bc30ffe8df9fe0528cdc28d319e6b8
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2023-02-28 16:12:39 -0300

    links a ig y youtube

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,15 +3,17 @@ import './Footer.css'
 import {AiFillInstagram} from "react-icons/ai"
 import {BsYoutube} from "react-icons/bs"
 import {IoLogoWhatsapp} from "react-icons/io"
+import { Link } from 'react-router-dom'
+
 
 export default function Footer() {
   return (
     <footer>
       <div className='redes-sociales-footer'>
-        <AiFillInstagram id='ig-footer'/>
-        <BsYoutube id='yt-footer'/>
+        <Link to='https://www.instagram.com/salute_drinkss2/' target={'_blank'}><AiFillInstagram id='ig-footer'/></Link>
+        <Link to='https://www.youtube.com/@salutetv4605' target={'_blank'}><BsYoutube id='yt-footer'/></Link>
         <IoLogoWhatsapp id='wp-footer'/>
-      </div>
+      </div> 
       <div className='dev-footer'>
         <span>- Salute Drinks 2023 -</span>
       </div>

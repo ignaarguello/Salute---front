36119a83c70fd52e20475de0628b2f0d73c476c4
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2023-03-01 01:35:20 -0300

    flex row para las cards de productos

--- src/pages/Productos/Productos.jsx
+++ src/pages/Productos/Productos.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import CardProd2 from '../../components/CardProducto/CardProd2'
 import CardProducto from '../../components/CardProducto/CardProducto'
 import './Productos.css'
 
@@ -6,6 +7,7 @@ export default function Productos() {
   return (
     <div id='productos-pagina-cont'>
       <CardProducto />
+      <CardProd2 />
     </div>
   )
 }

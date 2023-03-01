ae38f0b5d49693d9b7a10367fbf62f35fb0c56e3
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2023-03-01 13:50:46 -0300

    tercera card de prueba de productos

--- src/pages/Productos/Productos.jsx
+++ src/pages/Productos/Productos.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import CardProd2 from '../../components/CardProducto/CardProd2'
+import CardProd3 from '../../components/CardProducto/CardProd3'
 import CardProducto from '../../components/CardProducto/CardProducto'
 import './Productos.css'
 
 export default function Productos() {
   return (
     <div id='productos-pagina-cont'>
-      <CardProducto />
-      <CardProd2 />
+        <CardProducto />
+        <CardProd2 />
+        <CardProd3 />
+        <CardProducto />
+        <CardProd2 />
+        <CardProd3 />
+        <CardProducto />
+        <CardProd2 />
+        <CardProd3 />
+        <CardProducto />
+        <CardProd2 />
+        <CardProd3 />
     </div>
   )
 }

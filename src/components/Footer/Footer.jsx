7674b54a1753593cbbceba0eec081b7ed3ee4034
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2023-03-08 01:19:25 -0300

    arreglos del footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,9 +10,9 @@ export default function Footer() {
   return (
     <footer>
       <div className='redes-sociales-footer'>
-        <Link to='https://www.instagram.com/salute_drinkss2/' target={'_blank'}><AiFillInstagram id='ig-footer'/></Link>
-        <Link to='https://www.youtube.com/@salutetv4605' target={'_blank'}><BsYoutube id='yt-footer'/></Link>
-        <IoLogoWhatsapp id='wp-footer'/>
+        <Link to='https://www.instagram.com/salute_drinkss2/' target={'_blank'}><AiFillInstagram id='ig-footer'/><span>salute_drinkss2</span></Link>
+        <Link to='https://www.youtube.com/@salutetv4605' target={'_blank'}><BsYoutube id='yt-footer'/><span>salutetv4605</span></Link>
+        <Link to='/' target={'_blank'}><IoLogoWhatsapp id='wp-footer'/><span>+54 11 541U7333</span></Link>
       </div> 
       <div className='dev-footer'>
         <span>- Salute Drinks 2023 -</span>

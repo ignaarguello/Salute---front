e5c6a315d05f349ce668ae11ccb7ff84ebb1bee2
ignaarguello <igna.arguello2110@hotmail.com>
2023-03-09 13:34:21 -0300

    lpqtp vercel

---
+++ src/components/CardsHome/CardsHome.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './CardsHome.css'
+
+export default function CardsHome() {
+  return (
+    <div id='container-principal__CardsHome'>
+        <div className="card-producto__CardsHome"></div>
+        <div className="card-producto__CardsHome"></div>
+        <div className="card-producto__CardsHome"></div>
+        <div className="card-producto__CardsHome"></div>
+    </div>
+  )
+}

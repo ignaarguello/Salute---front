e0a5137cc7b460ba976e567f5abbb222605c844c
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2023-03-16 18:10:55 -0300

    zonas de entregas

--- src/pages/ZonasEntrega/ZonasEntrega.jsx
+++ src/pages/ZonasEntrega/ZonasEntrega.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
 import './ZonasEntrega.css'
+import {FaMapMarkerAlt} from 'react-icons/fa'
+import {TbTruckDelivery} from 'react-icons/tb'
 
 export default function ZonasEntrega() {
   return (
     <div id='zonasentrega-cont'>
       <div className='titulos-zonas'>
-        <span>Nuestras zonas de entrega</span>
+        <span className='animate__animated animate__slideInLeft animate__slower'>Nuestras zonas de entrega  <TbTruckDelivery /></span>
       </div>
       <div className='mapa-texto-cont'>
         <div className='mapa-zonas'>
-          <iframe src="https://www.google.com/maps/d/u/0/embed?mid=16cnMi0dh5EM0cmAK8CzGNTNkltTiWVo&ehbc=2E312F" width="640" height="480"></iframe>
+          <iframe className='mapaFrame' src="https://www.google.com/maps/d/u/0/embed?mid=16cnMi0dh5EM0cmAK8CzGNTNkltTiWVo&ehbc=2E312F"></iframe>
         </div>
         <div className='texto-zonas'>
-          <span>Zona verde - SIN COSTO</span>
-          <span>Zona negro - $250</span>
-          <span>Zona amarillo - $300</span>
-          <span>Zona violeta - $200</span>
-          <span>Zona azul - $150</span>
-          <span>Zona rojo - $150</span>
+          <div className='desc-texto-zonas'>
+            <span>Nos ubicamos en la zona de <span id='BernalEste'>Bernal Este</span>, sobre la calle Lavalle, a unas pocas cuadras del Registro Civil de Bernal.
+            Al momento de <span id='hacerPedidoTexto'>hacer tu pedido</span> podés optar por la opción de retirar en nuestro local o seleccionar la zona que te corresponda:</span>
+          </div>
+          <div className='zonas-color'>
+            <span><FaMapMarkerAlt id='z-rojo' /> Zona rojo - SIN COSTO</span>
+            <span><FaMapMarkerAlt id='z-verde' /> Zona verde - $150</span>
+            <span><FaMapMarkerAlt id='z-negro'/> Zona negro - $250</span>
+            <span><FaMapMarkerAlt id='z-amarillo'/> Zona amarillo - $300</span>
+            <span><FaMapMarkerAlt id='z-violeta'/> Zona violeta - $200</span>
+            <span><FaMapMarkerAlt id='z-azul'/> Zona azul - $150</span>
+          </div>
         </div>
       </div>
     </div>

153fa80f2c024221a96b9f1dbf1c0bb3f2e53d56
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2023-03-17 18:14:03 -0300

    salutetv pagina en proceso

--- src/pages/SaluteTV/SaluteTV.jsx
+++ src/pages/SaluteTV/SaluteTV.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './SaluteTV.css'
 
 export default function SaluteTV() {
   return (
-    <div>SaluteTV</div>
+    <div id='salutetv-pagina-cont'>
+      <div className='bienvenida-salutetv'>
+        <div className='titulo-salutetv'>
+          <span>SUSCRIBITE A SALUTE TV</span>
+        </div>
+        <div className='textoAnimado'>
+          <span>Divertite con videos de</span>
+          <ul>
+            <li>entretenimiento</li>
+            <li>historia</li>
+            <li>entrevistas</li>
+            <li>música</li>
+            <li>degustaciones</li>
+          </ul>
+        </div>
+      </div>
+      <div className='salutetv-segundoCont'>
+        <div className='info-canal'>
+          <Link to='https://www.youtube.com/@salutetv4605' target={'_blank'}><img id='logo-canalYT' src='./images/salutetv.jpg' alt='Logo del canal de YouTube' /></Link>
+          <div className='comentariosCont'>
+            <textarea className='inputComentario' placeholder='Dejanos tu comentario...' />
+            <div className='comentarios'>
+              <span id='nombreComentario'>Gonzalo Higuain</span>
+              <span id='textoComentario'>Me cagué de risa con la historia de la Quilmes jajjaj</span>
+            </div>
+          </div>
+        </div>
+        <div className='video-canalCont'>
+          <iframe className='video-canal' src="https://www.youtube.com/embed/8gTjxW7EV_w" title="SALUTE, TE CUENTA: LA HISTORIA DE LA CERVEZA QUILMES ! ! ! 🍻🍻🍻" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        </div>
+      </div>
+    </div>
   )
 }

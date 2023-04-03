ce10dfda5292c4776663d068f080d306bb822e1e
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2023-04-03 13:18:10 -0300

    item del carrito con botones para sumar/restar cantidad

--- src/components/CarritoItem/CarritoItem.jsx
+++ src/components/CarritoItem/CarritoItem.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CarritoItem.css'
 import { MdDeleteForever } from "react-icons/md";
+import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
 
 export default function CarritoItem(props) {
 
-    let {img, nombre, tipo, precio, fnBorrar} = props
+    let {img, nombre, tipo, precio, fnBorrar, cantidad, fnRestar, fnSumar} = props
+
+    let subtotalProd = cantidad * precio
 
     return (
         <div id='item-Cont'>
@@ -12,9 +15,16 @@ export default function CarritoItem(props) {
             <div className='info-itemCarrito'>
                 <span id='nombre-itemCarrito'>{nombre}</span>
                 <span id='tipo-itemCarrito'>{tipo}</span>
-                <span id='precio-itemCarrito'>${precio}</span>
+                <span id='precio-itemCarrito'>{cantidad} x ${precio} {subtotalProd !== precio && ` = ($${subtotalProd.toLocaleString('es-ES')})`}</span>
             </div>
-            <MdDeleteForever onClick={fnBorrar} className='borrar-itemCarrito'/>
+            <div className='btn-itemCarrito'>
+                <MdDeleteForever onClick={fnBorrar} className='borrar-itemCarrito'/>
+                <div className='btn2-itemCarrito'>
+                    <AiOutlineMinusCircle onClick={fnRestar} className='restar-itemCarrito' />
+                    <span id='cantidad-itemCarrito'>{cantidad}</span>
+                    <AiOutlinePlusCircle onClick={fnSumar} className='sumar-itemCarrito' />
+                </div>
+            </div>
         </div>
     )
 }

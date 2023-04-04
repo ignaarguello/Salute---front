3370994c0104943f8d6db7d9a912277ce4a891b4
ignaarguello <igna.arguello2110@hotmail.com>
2023-04-04 15:39:59 -0300

    linkeo de refs del footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -18,11 +18,11 @@ export default function Footer() {
           <img id='logo-salute__footer' src="../images/logo-salute.png" alt="" />
           <h2 id='titulo-links__footer'>Quick Links</h2>
           <div id='lista-contenedorLinks__footer'>
-            <Link className="link-ref__footer">1. Inicio</Link>
-            <Link className="link-ref__footer">2. Catalogo</Link>
-            <Link className="link-ref__footer">3. Iniciar Sesión</Link>
-            <Link className="link-ref__footer">4. Zonas de Entrega</Link>
-            <Link className="link-ref__footer">5. Salute TV</Link>
+            <Link to='/' className="link-ref__footer">1. Inicio</Link>
+            <Link to='/productos' className="link-ref__footer">2. Catalogo</Link>
+            <Link to='/ingresar' className="link-ref__footer">3. Iniciar Sesión</Link>
+            <Link to='/zonas-entrega' className="link-ref__footer">4. Zonas de Entrega</Link>
+            <Link to='/salute-tv' className="link-ref__footer">5. Salute TV</Link>
           </div>
         </div>
         <div className="containers-footer" id='container-B__footer'>

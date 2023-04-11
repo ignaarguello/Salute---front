2075220734d99bd8d979d1e73fdf9ad9417d31cd
ignaarguello <igna.arguello2110@hotmail.com>
2023-04-11 17:26:11 -0300

    carousel en 768px

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -4,31 +4,31 @@ import { useEffect } from 'react'
 import productosActions from '../../redux/actions/productosActions'
 import { useDispatch, useSelector } from 'react-redux'
 import { MdAddCircle } from "react-icons/md";
+import { useNavigate } from 'react-router-dom'
 
 export default function Carousel() {
 
+    const navigate = useNavigate()
     const dispatch = useDispatch()
-    const { traer_productos } = productosActions
-    const { productos } = useSelector(store => store.productos)
-    const { logeado, usuarioId } = useSelector(store => store.usuarios)
 
+    //?Acciones de redux:
+    const { traer_productos } = productosActions
+
+    //? Selectores de redux:
+    const { productos } = useSelector(store => store.productos)
+
+    //! Use Effect que trar los pruductos de la Base de datos
     useEffect(() => {
         dispatch(traer_productos())
     }, [])
 
-    const find_product = (producto) => {
-        if (logeado) {
-            let producto_encontrado = productos.find(e => e.nombre === producto)
-            console.log(producto_encontrado)
-        }
-    }
 
     return (
         <div id='container-general__carousel'>
             <h2 id='tituloProductos__carousel'>+Productos</h2>
             <div id='container-cards__carousel'>
                 {productos.map(producto =>
-                    <div className='card_carousel'>
+                    <div className='card_carousel' key={producto._id}>
                         <div className='container-image-card__carousel'>
                             <img className='imagen-card__carousel' src={producto.imagen} alt="Producto Salute" />
                         </div>
@@ -37,7 +37,7 @@ export default function Carousel() {
                             <h2 className='item-card__carousel'>{producto.tipo}</h2>
                             <div id='contenedor-boton-compra__carousel'>
                                 <h2 className='item-card__carousel-separado'>${producto.precio}</h2>
-                                <MdAddCircle id='button-agregar__carousel' onClick={()=>find_product(producto.nombre)} />
+                                <MdAddCircle id='button-agregar__carousel' onClick={() => navigate('/productos')} />
                             </div>
                         </div>
                     </div>
